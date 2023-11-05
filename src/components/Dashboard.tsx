88f134af33a7a4a5ac5852a0ed774bb856b53f49
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2023-11-05 23:00:54 +0530

    Fetched latest DB value

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Dashboard.css";
 import InfoTwoToneIcon from "@mui/icons-material/InfoTwoTone";
 import PersonIcon from "@mui/icons-material/Person";
+import { error } from "console";
+import { initializeApp } from "firebase/app";
+import {
+  getDatabase,
+  ref,
+  get,
+  set,
+  child,
+  update,
+  remove,
+  push,
+  onValue,
+} from "firebase/database";
+import { unsubscribe } from "diagnostics_channel";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFZjbBnAowTs7jBTlDqErserc_6jDZy8E",
+  authDomain: "health-guard-v1.firebaseapp.com",
+  databaseURL:
+    "https://health-guard-v1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "health-guard-v1",
+  storageBucket: "health-guard-v1.appspot.com",
+  messagingSenderId: "25738976542",
+  appId: "1:25738976542:web:6688d8de3f2c82a64d7d0f",
+  measurementId: "G-V6ZL7FGYW5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Creating object for getDatabase function
+const db = getDatabase(app);
 
 function Dashboard() {
+  // State to store the data from the Realtime Database
+  let [realtimeDatabaseData, setRealtimeDatabaseData] = useState(null);
+  // Function to subscribe to real-time updates
+  useEffect(() => {
+    const dbRef = ref(db, "Heartrate-Sensor-0/");
+
+    // Set up a listener for changes in the database
+    onValue(dbRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const resultData = snapshot.val();
+        console.log("resultData: ", resultData);
+
+        const resultDataKeys = Object.keys(resultData);
+        console.log("resultDataKeys: ", resultDataKeys);
+
+        if (resultDataKeys.length > 0) {
+          const lastKey = resultDataKeys[resultDataKeys.length - 1];
+          const lastChild = resultData[lastKey];
+
+          if (lastChild) {
+            const lastChildKeys = Object.keys(lastChild);
+            console.log("lastChildKeys: ", lastChildKeys);
+
+            if (lastChildKeys.length > 0) {
+              const lastNode =
+                lastChild[lastChildKeys[lastChildKeys.length - 1]];
+              console.log("Last Node: ", lastNode);
+
+              // Now you can access properties of the last node
+              if (lastNode && lastNode.heartRate) {
+                const lastHeartRate = lastNode.heartRate;
+                console.log("Last Heart Rate: ", lastHeartRate);
+                setRealtimeDatabaseData(lastHeartRate);
+              }
+            }
+          }
+        }
+      } else {
+        console.log("No data found");
+      }
+    });
+  }, []);
+
+  console.log("realtimeDatabaseData: ", realtimeDatabaseData);
   return (
     <div className="App">
       <div className="table-container">
@@ -27,6 +104,7 @@ function Dashboard() {
                 <th>Age</th>
                 <th>Phone</th>
                 <th>Status</th>
+                <th>Heartrate</th>
                 <th>View Details</th>
               </tr>
             </thead>
@@ -48,6 +126,7 @@ function Dashboard() {
                     <div className="status-chip">Normal</div>
                   </div>
                 </td>
+                <td>{realtimeDatabaseData}</td>
                 <td>
                   <InfoTwoToneIcon />
                 </td>
@@ -69,6 +148,7 @@ function Dashboard() {
                     <div className="status-chip">High BP</div>
                   </div>
                 </td>
+                <td>22</td>
                 <td>
                   <InfoTwoToneIcon />
                 </td>
